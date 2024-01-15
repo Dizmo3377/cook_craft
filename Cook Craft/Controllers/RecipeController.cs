c91637104e7380efe16c98f4b47c4bf1fde9626b
DizmoProd <dzozulya3377@gmail.com>
2024-01-15 16:41:42 +0000

    EditPage

--- Cook Craft/Controllers/RecipeController.cs
+++ Cook Craft/Controllers/RecipeController.cs
@@ -74,6 +74,88 @@ namespace Cook_Craft.Controllers
             return View(recipe);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var recipe = await _recipeRepository.GetRecipeAsync(id);
+
+            if (recipe == null) return NotFound();
+
+            var steps = recipe.Steps
+                .Select(step => step.Description)
+                .ToList();
+
+            var ingridients = recipe.Ingridients
+                .Select(ingredient => ingredient.Name)
+                .ToList();
+
+            var recipeViewModel = new EditRecipeViewModel
+            {
+                Id = recipe.Id,
+                Name = recipe.Name,
+                Description = recipe.Description,
+                PrepTime = recipe.PrepTime,
+                CookTime = recipe.CookTime,
+                URL = recipe.Image,
+                Rating = recipe.Rating,
+                Steps = steps,
+                Ingridients = ingridients,
+                AppUserId = recipe.AppUserId
+            };
+
+            return View(recipeViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditRecipeViewModel recipeVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Editing went wrong");
+                return View("Edit", recipeVM);
+            }
+
+            string photoResult;
+
+            if (recipeVM.Image != null)
+            {
+                var photo = await _photoService.AddPhotoAsync(recipeVM.Image);
+                photoResult = photo.Url.ToString();
+                recipeVM.URL = photoResult;
+            }
+            else
+            {
+                photoResult = recipeVM.URL;
+            }
+
+            var steps = recipeVM.Steps
+                .Select(step => new Step { Description = step })
+                .ToList();
+
+            var ingridients = recipeVM.Ingridients
+                .Select(ingredient => new Ingridient { Name = ingredient })
+                .ToList();
+
+            var recipe = new Recipe
+            {
+                Id = recipeVM.Id,
+                Name = recipeVM.Name,
+                Description = recipeVM.Description,
+                PrepTime = recipeVM.PrepTime,
+                CookTime = recipeVM.CookTime,
+                Image = photoResult,
+                Rating = recipeVM.Rating,
+                Steps = steps,
+                Ingridients = ingridients,
+                AppUserId = recipeVM.AppUserId
+            };
+
+            _recipeRepository.Update(recipe);
+
+            return RedirectToAction("Index", "Kitchen");
+        }
+
+
         public async Task<IActionResult> Delete(int id)
         {
             var recipe = await _recipeRepository.GetRecipeAsync(id);
--- Cook Craft/View Models/EditRecipeViewModel.cs
+++ Cook Craft/View Models/EditRecipeViewModel.cs
@@ -1,15 +1,16 @@
-﻿using Cook_Craft.Models;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Cook_Craft.View_Models;
 
-public class CreateRecipeViewModel
+public class EditRecipeViewModel
 {
+    public int Id { get; set; }
     [Required] public string Name { get; set; }
     public string? Description { get; set; }
     public double? PrepTime { get; set; }
     public double? CookTime { get; set; }
-    [Required] public IFormFile Image { get; set; }
+    public IFormFile? Image { get; set; }
+    public string URL { get; set; }
     public int Rating { get; set; }
     public List<string> Steps { get; set; }
     public List<string> Ingridients { get; set; }
